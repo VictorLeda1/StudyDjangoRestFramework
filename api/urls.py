df94070c4abb61b5b4519d3f833f828e2d3a310c
Victor Leda <victorleda.js@gmail.com>
2025-09-10 00:34:58 -0300

    Aula 4 (Serializer subclasses and Aggregated API data)

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list),
     path('products/<int:pk>/', views.product_list),
+    path('products/info/', views.product_info),
     path('orders/', views.order_list),
-
 ]
