f7764198fcf717b7d608735c5e2179f67ba2a5e2
Victor Leda <victorleda.js@gmail.com>
2025-10-28 21:58:04 -0300

    Aula 21 (Actions and Permissions in Django)

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, viewsets
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.pagination import (LimitOffsetPagination,
                                        PageNumberPagination)
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
@@ -47,10 +47,16 @@ class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     pagination_class = None
     filterset_class = OrderFilter
     filter_backends = [DjangoFilterBackend]
+    
+    @action(detail=False, methods=['get'], url_path='user-orders', permission_classes=[IsAuthenticated])
+    def user_orders(self, request): 
+        orders = self.get_queryset().filter(user=request.user)
+        serializer = self.get_serializer(orders, many=True)
+        return Response(serializer.data)
 
 class ProductInfoAPIView(APIView):
     def get(self, request):
